6a9eb91392d01fda567b7e2f084c2c0ce0429b25
FERNANDA ARAUJO DIAS ESPIRITO SANTO <fernanda.adesanto@senacsp.edu.br>
2023-09-20 21:00:45 -0300

    btn

--- Acme.WPF/MainWindow.xaml.cs
+++ Acme.WPF/MainWindow.xaml.cs
@@ -30,7 +30,23 @@ namespace Acme.WPF
         {
             //1º instanciando o objeto DTO
             FuncionarioDTO funcionario = new FuncionarioDTO();
-
+            funcionario.Nome = TxtNome.Text;
+            funcionario.Cpf = TxtCpf.Text;
+            funcionario.Email = TxtEmail.Text;
+            funcionario.Telefone = TxtTelefone.Text;
+            funcionario.Sexo = TxtSexo.Text;
+            funcionario.DtNascimento = Convert.ToDateTime(TxtDtNascimento.Text);
+            funcionario.DtAdmissao = Convert.ToDateTime(TxtAdmissao.Text);
+            funcionario.Logradouro = TxtLogradouro.Text;
+            funcionario.Numero = TxtNumero.Text;
+            funcionario.Complemento = TxtComplemento.Text;
+            funcionario.Bairro = TxtBairro.Text;
+            funcionario.Cidade = TxtCidade.Text;
+            funcionario.Uf = TxtUf.Text;
+            funcionario.Cep = TxtCep.Text;
+            funcionario.SalarioBruto = Convert.ToDecimal(TxtSalario.Text);
+            funcionario.IdDepartamento = Convert.ToInt32(CmbDepartamento.Text);
+            funcionario.IdCargo = Convert.ToInt32(CmbCargo.Text);
         }
     }
 }
